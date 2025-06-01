918c6ad36a5deb28053a54b8a29f5999489f7eea
Vladimir <bobhat1999@gmail.com>
2025-06-01 11:13:51 +0300

    problem with getByRole

--- AutoTests/Checkout/checkout_test.spec.ts
+++ AutoTests/Checkout/checkout_test.spec.ts
@@ -5,15 +5,15 @@ import { login } from '../helpers';
 
 test.only('should be able to checkout', async ({ page }) => {
     login(page, testData.usernames.valid, testData.password)
-    await page.waitForTimeout(2000);
-    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    await page.locator('[data-test="shopping-cart-link"]').click();
-    await page.waitForTimeout(2000);
-    await page.locator('#checkout').click();
-    await page.locator('#first-name').fill('Volodya');
-    await page.locator('#last-name').fill('Lanister');
-    await page.locator('#postal-code').fill('12345');
-    await page.locator('#continue').click();
-    await page.locator('#finish').click();
+    await expect(page.locator('[data-test="title"]')).toBeVisible()
+    await page.getByRole('button', { name: 'add-to-cart-sauce-labs-backpack'}).click();
+    await page.getByRole('link', {name: 'shopping-cart-link'}).click();
+    await expect(page.locator('#checkout')).toBeVisible()
+    await page.getByRole('button', { name: 'checkout'}).click();
+    await page.getByRole('textbox', { name:'firstName'}).fill('Volodya');
+    await page.getByRole('textbox', { name:'lastName'}).fill('Lanister');
+    await page.getByRole('textbox', { name:'postalCode'}).fill('12345');
+    await page.getByRole('button', {name: 'continue'}).click();
+    await page.getByRole('button', {name: 'finish'}).click();
     await expect(page.locator('[data-test="title"]')).toContainText('Checkout: Complete!');
 })
